bc060b89b7373510a940bec80c5c0436eb8640f2
Johnathan Clementi <johnclementi@148-100-182-111.FoxNet.marist.edu>
2018-04-15 17:23:16 -0400

    Initialize files.

---
+++ bluegill.js
@@ -0,0 +1,8 @@
+// Author: Johnathan Clementi & Jillian Preece
+//   email address: johnathan.clementi1@marist.edu
+//                  jillian.preece1@marist.edu
+// Prof: Matthew Johnson
+// Course: Data Visualization 
+// Date: 4/15/2018
+// Assignment: Final Project - Bluegill Sizes 
+// File name: bluegill.js
